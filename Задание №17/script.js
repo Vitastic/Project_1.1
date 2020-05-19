a746ad963eb2e40602960739ff9357592eb8442f
Vitastic <59787564+Vitastic@users.noreply.github.com>
2020-05-19 17:52:21 +0300

    Использовал Jquery

---
+++ Задание №17/script.js
@@ -0,0 +1,23 @@
+
+
+$(document).ready(function() {
+
+//расписания туров, выбрать тур, получить консультацию
+$('.main_nav nav ul li:eq(1) a, .main_btna, .main_btn:has(span)').on('click', function(event) {
+    event.preventDefault();
+    $('.overlay').fadeIn('slow');
+    $('.modal').slideDown('slow');
+});
+
+//закрытие на крестик
+$('.modal .close').on('click', function(event) {
+    event.preventDefault();
+    $('.overlay').fadeOut('slow');
+    $('.modal').slideUp('slow');
+});
+
+
+
+
+
+});
